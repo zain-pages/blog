b1a67ae2e33283b2cf59e62d6b8a2a84e688f599
e <l>
2017-09-12 11:10:49 +0800

    bat do update

--- hook.bat
+++ hook.bat
@@ -1,2 +1,3 @@
 @echo on
-hexo g && hexo d && git add . && git commit -m "bat do update" && git push && ping -n 30 127.1>nul
+hexo g && hexo d && git add . && git commit -m "bat do update" && git push && pause
+rem ping -n 30 127.1>nul
